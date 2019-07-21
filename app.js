09b51472288bfa8b3f8179b4009ee9a22dba7e0b
유광현 <ykh815@hotmail.com>
2019-07-21 22:41:11 +0900

    Change line color
    선택한 색상으로 line 그리기

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -33,9 +34,18 @@ function onMouseDown(event) {
     painting = true;
 }
 
+function handleClockClick(evnet) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => 
+    color.addEventListener("click", handleClockClick)
+);
